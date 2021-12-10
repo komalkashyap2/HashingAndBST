a97e5617a17c0fb2c2fb5929d10cefb8604b5b48
komalkashyap <komalkashyapmajri403@gmail.com>
2021-12-10 21:15:48 +0530

    [KOMAL KASHYAP] Added: Remove the words of sentence in paragraph

--- HashingAndBST/HashingAndBST/MapNode.cs
+++ HashingAndBST/HashingAndBST/MapNode.cs
@@ -83,7 +83,27 @@ namespace HashingAndBST
                 }
 
             }
-            //Display Linkedlist elements for particular key
+        public void Remove(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedListPosition(position);
+            bool itemFound = false;
+            KeyValue<K, V> founditem = default(KeyValue<K, V>);
+            foreach (KeyValue<K, V> keyValue in linkedList)
+            {
+                if (keyValue.Key.Equals(key))
+                {
+                    itemFound = true;
+                    founditem = keyValue;
+                }
+            }
+            if (itemFound)
+            {
+                linkedList.Remove(founditem);
+
+            }
+        }
+        //Display Linkedlist elements for particular key
             public void Display(K key)
             {
                 int position = GetArrayPosition(key);
